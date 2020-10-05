eb04b69712b0b6e50e2d0c3ab69ca1b118c75fd1
Shane D'Souza <sdsouza1996@gmail.com>
2020-10-05 20:45:13 +0500

    WP Simple Theme

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+function simple_theme_setup(){
+	// Featured Image Support
+	add_theme_support('post-thumbnails');
+
+	//menu
+	register_nav_menus( array(
+			'primary' =>  __('Primary Menu' )
+	));
+}
+add_action('after_setup_theme', 'simple_theme_setup');
+
+
+// Excerpt Length
+function set_excerpt_length(){
+	return 60;
+}
+add_filter('excerpt_length', 'set_excerpt_length');
+
+//Register siderbar
+function init_widgets(){
+	register_sidebar( array(
+		'name' 							=> 'Sidebar',
+		'id'  			 				=> 'sidebar',
+		'before_widget' 		=> '<div class="side-widget">',
+		'after_widget' 			=> '</div>',
+		'before_title' 			=> '<h3>',
+		'after_title'				=> '</h3>',
+	) );
+}
+add_action( 'widgets_init', 'init_widgets' );
